419e5e8c99735d6a9b622959ee441e27eccb1db6
Michael Christian <mbchristian01@gmail.com>
2023-11-03 18:58:32 -0500

    completed day 8

--- 2022/8.py
+++ 2022/8.py
@@ -1,13 +1,53 @@
-data = open('input.txt').readlines()
-count = 0
-print(data)
+data = open('input.txt').read().strip()
+lines = [x for x in data.split('\n')]
 
-for i in range(len(data)):
-  for j in range(len(data[i])):
-    if data[i][j] == '\n':
-      break
-    if i == 0 or i == len(data[i])-1 or j == 0 or j == len(data)-1:
+matrix = []
+for line in lines:
+  matrix.append(line)
+
+directions = [(-1,0),(1,0),(0,1),(0,-1)]
+rows = len(matrix)
+columns = len(matrix[0])
+count = 0
+
+for row in range(rows):
+  for column in range(columns):
+    vis = False
+    for (dr, dc) in directions:
+      rr = row
+      cc = column
+      ok = True
+      while True:
+        rr += dr
+        cc += dc
+        if not (0<=rr<rows and 0<=cc<columns):
+          break
+        if matrix[rr][cc] >= matrix[row][column]:
+          ok = False
+      if ok:
+        vis = True
+    if vis:
       count += 1
-    else:
-      print(data[i][j])
-print(count)
+print('p1:',count)
+
+topScore = 0
+for row in range(rows):
+  for column in range(columns):
+    score = 1
+    for (dr, dc) in directions:
+      distance = 1
+      rr = row + dr
+      cc = column + dc
+      while True:
+        if not (0<=rr<rows and 0<=cc<columns):
+          distance -= 1
+          break
+        if matrix[rr][cc] >= matrix[row][column]:
+          break
+        distance += 1
+        rr += dr
+        cc += dc
+      score *= distance
+    topScore = max(topScore, score)
+
+print('p2:',topScore)
