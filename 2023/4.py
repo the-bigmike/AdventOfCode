0efaec5354f3b122d786bf2925d1b770e2eb6046
Michael Christian <mbchristian01@gmail.com>
2023-12-04 08:54:27 -0600

    day 3 and 4

---
+++ 2023/4.py
@@ -0,0 +1,27 @@
+import re
+
+file = open("data/day4.txt")
+data = file.read()
+lines = [x for x in data.split('\n')]
+
+sum1 = 0
+cards = [1 for i in lines]
+for i, line in enumerate(lines):
+  score = 0
+  info = re.split(":\s|\s\|\s", line)
+  winningNumbers = re.findall("\d+", info[1])
+  values = re.findall("\d+", info[2])
+  for number in winningNumbers:
+    if(values.__contains__(number)):
+      score += 1
+  if(score > 0):
+    sum1 += pow(2, score-1)
+    j = 0
+    while j < score and j+1 < len(lines):
+      j += 1
+      cards[j+i] += cards[i]
+
+print(f'Part 1: {sum1}')
+print(f'Part 2: {sum(cards)}')
+
+file.close()
